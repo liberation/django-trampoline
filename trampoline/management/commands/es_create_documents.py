fd252ccfc36a4142eb76bdbda36b4aa93bf22ac1
Laurent Guilbert <laurent.guilbert91@gmail.com>
2016-10-25 14:55:08 +0200

    Tryout multiprocessing.

--- trampoline/management/commands/es_create_documents.py
+++ trampoline/management/commands/es_create_documents.py
@@ -2,9 +2,9 @@
 Management command for trampoline.
 """
 from __future__ import print_function
-from concurrent.futures import as_completed
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
 from optparse import make_option
+import itertools
 import logging
 import sys
 
@@ -13,6 +13,7 @@ from tqdm import tqdm
 from elasticsearch_dsl import Index
 
 from django.contrib.contenttypes.models import ContentType
+from django.db import connection
 
 from trampoline.management.base import ESBaseCommand
 from trampoline.tasks import es_index_object
@@ -24,23 +25,40 @@ from trampoline.tasks import STATUS_INDEXED
 logger = logging.getLogger(__name__)
 
 
+def index_object(object_id, content_type_id, target_name, dry_run):
+    connection.close()
+    result = {
+        'status': STATUS_INDEXED,
+        'object_id': object_id,
+        'content_type_id': content_type_id,
+    }
+    if not dry_run:
+        try:
+            result['status'] = es_index_object.run(
+                target_name,
+                content_type_id,
+                object_id,
+                fail_silently=False
+            )
+        except Exception as exc:
+            result['status'] = STATUS_FAILED
+            result['exc'] = exc
+    return result
+
+
+def star_index_object(args):
+    return index_object(*args)
+
+
 class Command(ESBaseCommand):
     help = (
         "Create documents on {0}{1}INDEX_NAME{2} based on the method "
         "{0}get_indexable_queryset{2} on the related models."
     ).format(ESBaseCommand.BOLD, ESBaseCommand.UNDERLINE, ESBaseCommand.RESET)
 
-    MAX_THREADS_DEFAULT = 4
-
     option_list = ESBaseCommand.option_list + (
         ESBaseCommand.options['index_name'],
         ESBaseCommand.options['target_name'],
-        make_option(
-            '--threads',
-            dest='max_threads',
-            default=MAX_THREADS_DEFAULT,
-            help="Number of threads."
-        ),
         make_option(
             '--cleanup',
             dest='cleanup',
@@ -91,23 +109,32 @@ class Command(ESBaseCommand):
                 desc=desc
             )
 
-            max_threads = self.get_max_threads()
-            with ThreadPoolExecutor(max_workers=max_threads) as executor:
-                for object_id in object_ids:
-                    future = executor.submit(
-                        self.index_object,
-                        content_type_id,
-                        object_id
-                    )
-                    future.add_done_callback(self.done_callback)
+            pool = Pool(None, maxtasksperchild=100)
+            imap_results = pool.imap_unordered(
+                star_index_object,
+                itertools.izip(
+                    object_ids,
+                    itertools.repeat(content_type_id),
+                    itertools.repeat(self.target_name),
+                    itertools.repeat(self.dry_run)
+                )
+            )
+            try:
+                for result in imap_results:
+                    self.indexation_callback(result)
+            except KeyboardInterrupt:
+                pool.terminate()
+                pool.join()
+            else:
+                pool.close()
+                pool.join()
+
             self.progress_bar.close()
 
         self.log_file.close()
         self.print_success("Indexation completed.")
 
-    def done_callback(self, task):
-        result = task.result()
-
+    def indexation_callback(self, result):
         status = result['status']
         if status in self.progress_status:
             self.progress_status[status] += 1
@@ -115,7 +142,7 @@ class Command(ESBaseCommand):
         exc = result.get('exc')
         if exc is not None:
             print(
-                "FAILED: pk {0} (content_type {1})\n"
+                "FAILED: pk {0} (content_type {1})"
                 .format(
                     result['content_type_id'],
                     result['object_id'],
@@ -153,32 +180,3 @@ class Command(ESBaseCommand):
             )
         )
         return desc
-
-    def get_max_threads(self):
-        try:
-            max_threads = int(self.max_threads)
-        except:
-            max_threads = self.MAX_THREADS_DEFAULT
-        else:
-            if max_threads < 1 or max_threads > 10:
-                max_threads = self.MAX_THREADS_DEFAULT
-        return max_threads
-
-    def index_object(self, content_type_id, object_id):
-        result = {
-            'status': STATUS_INDEXED,
-            'object_id': object_id,
-            'content_type_id': content_type_id,
-        }
-        if not self.dry_run:
-            try:
-                result['status'] = es_index_object.run(
-                    self.target_name,
-                    content_type_id,
-                    object_id,
-                    fail_silently=False
-                )
-            except Exception as exc:
-                result['status'] = STATUS_FAILED
-                result['exc'] = exc
-        return result
